d2541767fa7bc0c723d48784b95657a3f1f5ac54
Elliot Koh <appdev.moose@gmail.com>
2019-12-05 12:58:29 +0800

    Added scroll for event detail text view

--- app/src/main/java/javanesecoffee/com/blink/events/EventDetailActivity.java
+++ app/src/main/java/javanesecoffee/com/blink/events/EventDetailActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -73,6 +74,7 @@ public class EventDetailActivity extends BlinkActivity implements BLinkEventObse
         alsoAttendingTextView = findViewById(R.id.alsoAttendingTextView);
         eventRegisteredTextView = findViewById(R.id.eventRegisteredTextView);
 
+        eventDescription.setMovementMethod(new ScrollingMovementMethod());
 
         //button
         eventRegisterButton = findViewById(R.id.event_detail_register_button);
